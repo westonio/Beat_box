365f5648724010ba1395fe15a7302c216b38162e
Weston Schutt <wtschutt@gmail.com>
2023-05-10 18:31:51 -0600

    Add initial tests for Node class

---
+++ spec/node_spec.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+require './lib/node'
+
+RSpec.describe Node do
+  it 'is an instance of' do
+    node = Node.new("splooie")
+
+    expect(node).to be_an_instance_of(Node)
+  end
+
+  it 'has data' do
+    node = Node.new("splooie")
+
+    expect(node.data).to eq("splooie")
+  end
+
+  it 'has a next_node = nil' do
+    node = Node.new("splooie")
+
+    expect(node.next_node).to eq nil
+  end
+end
