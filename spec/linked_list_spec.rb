5e9aebd988c19e7086a78b9271a1ee1c756bfe30
Weston Schutt <wtschutt@gmail.com>
2023-05-10 20:04:16 -0600

    Add test for appending multiple nodes

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -4,38 +4,48 @@ require 'rspec'
 require 'pry'
 
 
-RSpec.describe Linked_List do
+RSpec.describe LinkedList do
   it 'is an instance of' do
-    list = Linked_List.new
+    list = LinkedList.new
 
-    expect(list).to be_an_instance_of(Linked_List)
+    expect(list).to be_an_instance_of(LinkedList)
   end
 
   it 'can append nodes' do
-    list = Linked_List.new
+    list = LinkedList.new
 
     list.append("doop")
     expect(list.head).not_to eq nil
+    expect(list.head.data).to eq("doop")
   end
 
   it 'has a next_node of nil' do
-    list = Linked_List.new
+    list = LinkedList.new
 
     list.append("doop")
     expect(list.head.next_node).to eq nil
   end
 
   it 'has a count of nodes in the list' do
-    list = Linked_List.new
+    list = LinkedList.new
 
     list.append("doop")
     expect(list.count).to eq 1
   end
 
   it 'can return the nodes data as a string' do
-    list = Linked_List.new
+    list = LinkedList.new
 
     list.append("doop")
     expect(list.to_string).to eq("doop")
+  end
+
+  it 'can append multiple nodes' do
+    list = LinkedList.new
+
+    list.append("doop")
+    list.append("beep")
+    expect(list.head.data).to eq("doop")
+    expect(list.head.next_node.data).to eq("beep")
   end
 end
