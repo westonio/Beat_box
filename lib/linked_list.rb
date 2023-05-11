fca98b62d3dbe25f70c678ecb5176bfdc3f7d97b
Weston Schutt <wtschutt@gmail.com>
2023-05-10 20:19:28 -0600

    Update append method to allow multiple linked nodes

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,15 +1,21 @@
-class Linked_List
+class LinkedList
   attr_reader :head, :count
 
   def initialize
     @head = nil
-    @count = 0
+    @count = 0 # This is incorrect usage based off of requirements, need to re-write as a separate method
   end
 
   def append(sound)
-    @count += 1
-    @head = Node.new(sound)
+    if @head == nil
+      @count += 1
+      @head = Node.new(sound)
+    else
+      @count += 1
+      @head.add_node(sound)
+    end
   end
+
 
   def to_string
     @head.data.to_s
