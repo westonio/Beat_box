acb3ebb7be683fc5d34de7d90c2991605ba44dee
Weston Schutt <wtschutt@gmail.com>
2023-05-11 17:34:14 -0600

    Correct count from an attribute to a method that counts all nodes

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -3,20 +3,30 @@ class LinkedList
 
   def initialize
     @head = nil
-    @count = 0
   end
 
+  def count
+    current_count = 0
+    current_node = @head
+    if current_node == nil
+      current_count
+    else
+      until current_node.next_node == nil
+        current_node = current_node.next_node
+        current_count += 1
+      end
+      current_count + 1 # This +1 accounts for the tail node, which stopped the until statement above
+    end 
+  end
 
   def append(sound)
     if @head == nil
-      @count += 1
       @head = Node.new(sound)
     else
       current_node = @head
       until current_node.next_node == nil
         current_node = current_node.next_node
       end
-      @count += 1
       current_node.add_node(sound)
     end
   end
