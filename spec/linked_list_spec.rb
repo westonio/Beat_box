43fd2fb1bf29c8fa536bed2bd8169c88c7310418
Weston Schutt <wtschutt@gmail.com>
2023-05-13 09:46:24 -0600

    Add test for new feature .find

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -13,39 +13,39 @@ RSpec.describe LinkedList do
 
   it 'can append nodes' do
     list = LinkedList.new
-
     list.append("doop")
+
     expect(list.head).not_to eq nil
     expect(list.head.data).to eq("doop")
   end
 
   it 'has a next_node of nil' do
     list = LinkedList.new
-
     list.append("doop")
+
     expect(list.head.next_node).to eq nil
   end
 
   it 'can count nodes in the linked list' do
     list = LinkedList.new
-
     list.append("doop")
+
     expect(list.count).to eq 1
   end
 
   it 'can return the nodes data as a string' do
     list = LinkedList.new
-
     list.append("doop")
+
     expect(list.to_string).to eq("doop")
   end
 
   it 'can append multiple nodes' do
     list = LinkedList.new
-
     list.append("doop")
     list.append("beep")
     list.append("duub")
+
     expect(list.head.data).to eq("doop")
     expect(list.head.next_node.data).to eq("beep")
     expect(list.head.next_node.next_node.data).to eq("duub")
@@ -54,28 +54,28 @@ RSpec.describe LinkedList do
 
   it 'can count multiple nodes' do
     list = LinkedList.new
-
     list.append("doop")
     list.append("beep")
     list.append("duub")
     list.append("boot")
+
     expect(list.count).to eq 4
   end
 
   it 'can return all nodes data as a single string' do
     list = LinkedList.new
-
     list.append("doop")
     list.append("beep")
     list.append("duub")
     list.append("boot")
+
     expect(list.to_string).to eq("doop beep duub boot")
   end
 
   it 'can prepend a node at the beginning' do
     list = LinkedList.new
-
     list.append("doop")
+
     list.prepend("beep")
     expect(list.head.data).to eq("beep")
     expect(list.head.next_node.data).to eq("doop")
@@ -83,10 +83,10 @@ RSpec.describe LinkedList do
 
   it 'can insert at a given position in the linked list' do
     list = LinkedList.new
-
     list.append("doop")
     list.append("beep")
     list.append("duub")
+
     list.insert(1,"siip")
     expect(list.to_string).to eq("doop siip beep duub")
     expect(list.head.next_node.data).to eq("siip")
@@ -94,7 +94,6 @@ RSpec.describe LinkedList do
 
   it 'gives an error if trying to insert node at invalid position' do
     list = LinkedList.new
-
     list.append("doop")
     list.append("beep")
     
@@ -103,19 +102,30 @@ RSpec.describe LinkedList do
 
   it 'insert can insert a node at the beginning' do
     list = LinkedList.new
-
     list.append("doop")
+
     list.insert(0,"siip")
     expect(list.to_string).to eq("siip doop")
   end
 
   it 'can insert a node at the end' do
     list = LinkedList.new
-    
     list.append("doop")
     list.append("beep")
     list.append("duub")
+
     list.insert(3,"siip")
     expect(list.to_string).to eq("doop beep duub siip")
+  end
+
+  it 'can find a node position and return multiple nodes' do
+    list = LinkedList.new
+    list.append("doop")
+    list.append("beep")
+    list.append("duub")
+    list.append("siip")
+    
+    find = list.find(2,2)
+    expect(find).to eq("duub siip")
   end
 end
