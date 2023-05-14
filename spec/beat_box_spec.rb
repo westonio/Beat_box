0312ad84243cfbc6e48a2d724efc44cf279993ac
Weston Schutt <wtschutt@gmail.com>
2023-05-14 10:59:24 -0600

    Add test for initialized LinkedList from BeatBox class

--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -6,9 +6,15 @@ require 'pry'
 
 
 RSpec.describe BeatBox do
-  it 'is an instance ofbeat' do
+  it 'is an instance of BeatBox' do
     bb = BeatBox.new
 
     expect(bb).to be_an_instance_of(BeatBox)
+  end
+
+  it 'has a list' do
+    bb = BeatBox.new
+
+    expect(bb.list).to be_an_instance_of(LinkedList)
   end
 end
