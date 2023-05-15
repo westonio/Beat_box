9e49032a1d4ed2544c7f201227c00aef9a887c8b
Weston Schutt <wtschutt@gmail.com>
2023-05-14 18:31:07 -0600

    Add .move_position method and refactor .insert method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -49,26 +49,30 @@ class LinkedList
     @head = new_head # re-assign head to this new node
   end
 
-  def insert(position, sound)
+  # Creating .move_position method to reduce repeated code for .insert and .find
+  def move_position(number)
     current_node = @head
-    if current_node == nil && position != 0 || position > count
+    
+    (number - 1).times do
+      current_node = current_node.next_node
+    end
+    current_node
+  end
+
+  def insert(position, sound)
+    if position > count
       "The list is currently #{count} nodes long. You cannot insert a node here."
-    elsif position == 0 && current_node == nil
-      append(sound)
-    elsif position == 0 && current_node != nil
+    elsif position == 0 
       prepend(sound)
     else
-      position = position - 1 # This ensures we add a node after the given node (e.g. the first node as the head is zero)
-      position.times do
-        current_node = current_node.next_node
-      end
+      current_node = move_position(position)
       
-      if current_node.next_node == nil
-        new_node = current_node.add_node(sound)
-      else
-        pointer_node = current_node.next_node
-        new_node = current_node.add_node(sound)
-        new_node.add_node(pointer_node)
+      if current_node.next_node.nil? # if at tail, just add
+        current_node.add_node(sound)
+      else # if not at the tail
+        pointer_node = current_node.next_node 
+        new_node = current_node.add_node(sound) #adds node after current node
+        new_node.add_node(pointer_node) # attaches previous node's next_nod as the next node for inserted
       end
     end
   end
